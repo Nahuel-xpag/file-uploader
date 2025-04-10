5979920fb7fb4af622bd24bfdd4e3bbfe3b66459
Nahuel-xpag <agustin.nahuu@gmail.com>
2025-04-10 19:18:47 +0000

    setup

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const passport = require('passport');
+const path = require('node:path');
+const expressSession = require('express-session');
+//const { PrismaSessionStore } = require('@quixo3/prisma-session-store');
+//const { PrismaClient } = require('@prisma/client');
+
+const app = express()
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(expressSession({ secret: 'secreto uno', resave: false, saveUninitialized: false}))
+app.use(passport.session());
+app.use(express.urlencoded({extended: false}));
+/*app.use(expressSession({
+    cookie: {
+        maxAge: 7 * 24 * 60 * 60 * 1000
+    },
+    secret: 'somesecret',
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(
+        new PrismaClient(),
+        {
+            checkPeriod: 2 * 60 * 1000,
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }
+    )
+})
+);*/
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.listen(3000, () => console.log("Server listening on port three thousand"))
